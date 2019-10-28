e9889db66b8bac8f838ec4d5a689eea573a6135a
Saif Masadeh <saifmasadeh@gmail.com>
2019-10-28 15:27:14 -0500

    removed some unneeded configs

--- code_in_modules-terragrunt/non-prod/us-east-1/variables.tf
+++ code_in_modules-terragrunt/non-prod/us-east-1/variables.tf
@@ -7,9 +7,9 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "fd-dealercodes-lambda" {
+variable "xx-dealercodes-lambda" {
   type = string
-  default ="arn:aws:iam::489430075229:role/acct-managed/fd-dealercodes-lambda"
+  default ="arn:aws:iam::xxxx:role/acct-managed/xx-dealercodes-lambda"
 }
 
 #
@@ -17,10 +17,6 @@ variable "fd-dealercodes-lambda" {
 #
 provider "aws" {
   region = "us-east-1"
-}
-
-provider "alks" {
-  url = "https://alks.coxautoinc.com/rest"
 }
 
 terraform {
